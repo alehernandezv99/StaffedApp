c83c3818d2cdc983480c41e5a5135802527250d5
alejandro hernandez <alehernandezv99@gmail.com>
2019-07-24 03:06:48 -0400

    Added Functionality to the search bar

--- severSide/index.js
+++ severSide/index.js
@@ -2,7 +2,29 @@ const express = require('express')
 const app = express()
 const algoliasearch = require('algoliasearch/lite');
 const client = algoliasearch('D6DXHGALTD', 'fad277b448e0555dfe348a06cc6cc875');
-const index = client.initIndex('your_index_name');
+const index = client.initIndex('projects');
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../functions/freelanceapp-78578-firebase-adminsdk-rzifn-e2e9c82390.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://freelanceapp-78578.firebaseio.com"
+});
+
+
+let records = []
+
+admin.firestore().collection("projects").get().then(async(projects) => {
+  projects.forEach(doc => {
+    let obj = doc.data()
+    obj.objectID = doc.id;
+    records.push(obj)
+  })
+
+  await index.saveObjects(records);
+  console.log("Everything pushed to algolia");
+})
  
 app.get('/', function (req, res) {
   res.send('Hello World')
