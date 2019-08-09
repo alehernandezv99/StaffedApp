e718f5fffd5141a66329eca4e6b0056904a65f03
alejandro hernandez <alehernandezv99@gmail.com>
2019-08-09 16:05:35 -0400

    Fixing bugs Company CV Type

--- src/structure/body/profile/staffCreator/staffCreator.js
+++ src/structure/body/profile/staffCreator/staffCreator.js
@@ -3,6 +3,7 @@ import "./staffCreator.css";
 import { Button, Position, Classes, Slider, Drawer, DateInput} from "@blueprintjs/core";
 import CVcontent from "../CVcontent";
 import firebase from "../../../../firebaseSetUp";
+import $ from "jquery";
 
 export default class StaffCreator extends React.Component{
     constructor(props){
@@ -20,12 +21,94 @@ export default class StaffCreator extends React.Component{
                 contact:[],
                 editable:true,
                 order:[1,2,3,4,5,6]
+            },
+            inputs:{
+                description:{
+                    title:"",
+                    description:""
+                },
+                experience:{
+                    title:"",
+                    description:""
+                },
+                education:{
+                    title:"",
+                    description:""
+                },
+                portfolio:{
+                    title:"",
+                    description:""
+                },
+                skills:{
+                    title:"",
+                    description:""
+                },
+                expertise:{
+                    title:"",
+                    description:""
+                },
+                contact:{
+                    title:"",
+                    description:""
+                },
+
             }
         }
     }
 
-    
+    toggleAddContent = (id) => {
+        $(id).slideToggle("fast")
+    }
 
+    addElement = (field, obj,) => {
+        this.setState(state => {
+            //CV
+            let base = state.CV;
+            let base2 = base[field];
+            base2.push(obj);
+            base[field] = base2;
+
+            //Input
+            let inputs = state.inputs;
+            let inputs1 = inputs[field];
+            let base3 = inputs1["title"];
+            base3 = ""
+            let base4 = inputs1["description"];
+            base4 = "";
+            inputs1["title"] = base3;
+            inputs1["description"] = base4;
+            inputs[field] = inputs1
+            return {
+                CV:base,
+                inputs:inputs
+            }
+        })
+    }
+
+    changeInputValue = (field, type, value) => {
+        if(this._mounted){
+            this.setState(state => {
+                let base = state.inputs;
+                let base2 = base[field];
+                let base3 = base2[type]
+                base3 = value
+                base2[type] = base3;
+                base[field] = base2;
+
+                return {
+                    inputs:base
+                }
+            })
+        }
+    }
+
+    componentDidMount() {
+        this._mounted = true
+    }
+
+    componentWillUnmount(){
+        this._mounted = false;
+    }
 
     render(){
         return (
@@ -85,7 +168,23 @@ export default class StaffCreator extends React.Component{
             
                                    )
                                        }):null}
-                                   {this.state.CV.editable === true? <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.openEditPanel("add",this.state.CV.id,"experience");}}><i className="material-icons align-middle">add</i> <span>Add</span></button>: null}
+                                   {this.state.CV.editable === true?
+                                     <div> <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.toggleAddContent("#experience-form")}}><i className="material-icons align-middle">add</i> <span>Add</span></button>
+                                     <div className="container" id="experience-form" style={{display:"none"}}>
+                                         <div className="form-group" >
+                                            <label>Title</label> 
+                                            <input type="text" value={this.state.inputs.experience.title} onChange={(e) => {this.changeInputValue("experience","title",e.target.value)}} className="form-control" />
+                                         </div>
+                                         <div className="form-group">
+                                            <label>Education</label> 
+                                            <textarea value={this.state.inputs.experience.description} onChange={(e) => {this.changeInputValue("experience","description",e.target.value)}} className="form-control" ></textarea>
+                                         </div>
+                                         <div className="form-group">
+                                             <button type="button" onClick={() => {this.addElement("experience",{title:this.state.inputs.experience.title,text:this.state.inputs.experience.description})}} className="btn btn-custom-1 btn-sm"><i className="material-icons align-middle">add</i> Add</button>
+                                         </div>
+                                     </div>
+                                     </div>
+                                     : null}
                                 </div>
             
                                 </div> 
@@ -114,15 +213,18 @@ export default class StaffCreator extends React.Component{
                        )
                            }):null}
                        {this.state.CV.editable === true?
-                        <div> <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.openEditPanel("add",this.state.CV.id,"education");}}><i className="material-icons align-middle">add</i> <span>Add</span></button>
-                        <div className="container">
-                            <div className="form-group" id="education-form">
+                        <div> <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.toggleAddContent("#education-form")}}><i className="material-icons align-middle">add</i> <span>Add</span></button>
+                        <div className="container" id="education-form" style={{display:"none"}}>
+                            <div className="form-group" >
                                <label>Title</label> 
-                               <input type="text" className="form-control" />
+                               <input type="text" value={this.state.inputs.education.title} onChange={(e) => {this.changeInputValue("education","title",e.target.value)}} className="form-control" />
                             </div>
                             <div className="form-group">
                                <label>Education</label> 
-                               <input type="text" className="form-control" />
+                               <textarea value={this.state.inputs.education.description} onChange={(e) => {this.changeInputValue("education","description",e.target.value)}} className="form-control" ></textarea>
+                            </div>
+                            <div className="form-group">
+                                <button type="button" className="btn btn-custom-1 btn-sm"><i className="material-icons align-middle">add</i> Add</button>
                             </div>
                         </div>
                         </div>
@@ -152,7 +254,23 @@ export default class StaffCreator extends React.Component{
 
                                )
                               }):null}
-                               {this.state.CV.editable === true? <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.openEditPanel("add",this.state.CV.id,"portfolio");}}><i className="material-icons align-middle">add</i> <span>Add</span></button>: null}
+                               {this.state.CV.editable === true? 
+                               <div> <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.toggleAddContent("#portfolio-form")}}><i className="material-icons align-middle">add</i> <span>Add</span></button>
+                               <div className="container" id="portfolio-form" style={{display:"none"}}>
+                                   <div className="form-group" >
+                                      <label>Title</label> 
+                                      <input type="text" value={this.state.inputs.portfolio.title} onChange={(e) => {this.changeInputValue("portfolio","title",e.target.value)}} className="form-control" />
+                                   </div>
+                                   <div className="form-group">
+                                      <label>Education</label> 
+                                      <textarea value={this.state.inputs.portfolio.description} onChange={(e) => {this.changeInputValue("portfolio","description",e.target.value)}} className="form-control" ></textarea>
+                                   </div>
+                                   <div className="form-group">
+                                       <button type="button" className="btn btn-custom-1 btn-sm"><i className="material-icons align-middle">add</i> Add</button>
+                                   </div>
+                               </div>
+                               </div>
+                               : null}
                              </div>
                             </div>
                               )
@@ -179,7 +297,23 @@ export default class StaffCreator extends React.Component{
 
                        )
                            }):null}
-                                   {this.state.CV.editable === true? <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.openEditPanel("add",this.state.CV.id,"skills");}}><i className="material-icons align-middle">add</i> <span>Add</span></button>: null}
+                                   {this.state.CV.editable === true? 
+                                   <div> <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.toggleAddContent("#skills-form")}}><i className="material-icons align-middle">add</i> <span>Add</span></button>
+                                   <div className="container" id="skills-form" style={{display:"none"}}>
+                                       <div className="form-group" >
+                                          <label>Title</label> 
+                                          <input type="text" value={this.state.inputs.skills.title} onChange={(e) => {this.changeInputValue("skills","title",e.target.value)}} className="form-control" />
+                                       </div>
+                                       <div className="form-group">
+                                          <label>Education</label> 
+                                          <textarea value={this.state.inputs.skills.description} onChange={(e) => {this.changeInputValue("skills","description",e.target.value)}} className="form-control" ></textarea>
+                                       </div>
+                                       <div className="form-group">
+                                           <button type="button" className="btn btn-custom-1 btn-sm"><i className="material-icons align-middle">add</i> Add</button>
+                                       </div>
+                                   </div>
+                                   </div>
+                                   : null}
                                    </div>
                                </div>
                                 )
@@ -206,7 +340,23 @@ export default class StaffCreator extends React.Component{
 
                                        )
                                     }):null}
-                                   {this.state.CV.editable === true? <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.openEditPanel("add",this.state.CV.id,"expertise");}}><i className="material-icons align-middle">add</i> <span>Add</span></button>: null}
+                                   {this.state.CV.editable === true?
+                                    <div> <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.toggleAddContent("#expertise-form")}}><i className="material-icons align-middle">add</i> <span>Add</span></button>
+                                    <div className="container" id="expertise-form" style={{display:"none"}}>
+                                        <div className="form-group" >
+                                           <label>Title</label> 
+                                           <input type="text" value={this.state.inputs.expertise.title} onChange={(e) => {this.changeInputValue("expertise","title",e.target.value)}} className="form-control" />
+                                        </div>
+                                        <div className="form-group">
+                                           <label>Education</label> 
+                                           <textarea value={this.state.inputs.expertise.description} onChange={(e) => {this.changeInputValue("expertise","description",e.target.value)}} className="form-control" ></textarea>
+                                        </div>
+                                        <div className="form-group">
+                                            <button type="button" className="btn btn-custom-1 btn-sm"><i className="material-icons align-middle">add</i> Add</button>
+                                        </div>
+                                    </div>
+                                    </div>
+                                     : null}
                                   </div>
                                  </div>
                                 )
@@ -216,7 +366,7 @@ export default class StaffCreator extends React.Component{
                                 return (
                                     <div className="card mt-3 mb-3" key={element}>
                                       <div className="card-header" style={{position:"relative"}}>
-                                        <a className="card-link" data-toggle="collapse" href="#contact"> Contact</a>
+                                        <a className="card-link" data-toggle="collapse" href="#contact-cv"> Contact</a>
 
                                         {this.state.CV.editable === true?
                                         <div className="btn-group btns-change-order">
@@ -226,14 +376,30 @@ export default class StaffCreator extends React.Component{
                                         :null}
                                      </div>
 
-                                    <div className="collapse show" id="contact">
+                                    <div className="collapse show" id="contact-cv" >
                                        {this.state.CV.contact.length > 0?this.state.CV.contact.map((element,i) => {
                                       return (
                                         <CVcontent editable={this.state.CV.editable} key={i} edit={() => {this.openEditPanel("update",this.state.CV.id,"contact",i,element.title,element.text)}} delete={() => {this.deleteContent("contact",i); }} title={element.title} text={element.text}/>
 
                                         )
                                        }):null}
-                                        {this.state.CV.editable === true? <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.openEditPanel("add",this.state.CV.id,"contact");}}><i className="material-icons align-middle">add</i> <span>Add</span></button>: null}
+                                        {this.state.CV.editable === true? 
+                                        <div> <button type="button" className="btn btn-custom-3 btn-sm m-2" onClick={() => {this.toggleAddContent("#contact-form")}}><i className="material-icons align-middle">add</i> <span>Add</span></button>
+                                        <div className="container" id="contact-form" style={{display:"none"}}>
+                                            <div className="form-group" >
+                                               <label>Title</label> 
+                                               <input type="text" value={this.state.inputs.contact.title}  onChange={() => {this.changeInputValue("contact","title")}} className="form-control" />
+                                            </div>
+                                            <div className="form-group">
+                                               <label>Education</label> 
+                                               <textarea value={this.state.inputs.contact.description} onChange={() => {this.changeInputValue("contact","description")}} className="form-control" ></textarea>
+                                            </div>
+                                            <div className="form-group">
+                                                <button type="button" className="btn btn-custom-1 btn-sm"><i className="material-icons align-middle">add</i> Add</button>
+                                            </div>
+                                        </div>
+                                        </div>
+                                        : null}
                                         </div>
                         
                                    </div>
@@ -242,6 +408,9 @@ export default class StaffCreator extends React.Component{
                         })}
                     
                 </div>
+                 <div className="form-group">
+                     <button type="button" className="btn btn-custom-1 "><i className="material-icons align-middle">add</i> Create</button>
+                 </div>
               </div>
             </div>
            </Drawer>
