2bff61fcd998f90e3b2ecf3563d77e38b10faeba
Alejandro Hernandez <48463924+alehernandezv99@users.noreply.github.com>
2019-10-09 00:01:49 -0400

    Safe

--- testApiCalls/index.js
+++ testApiCalls/index.js
@@ -5,8 +5,8 @@ const qs = require("querystring");
 
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
-    'client_id': 'AVTUrNQsFoxsTJwhbxJYvSiKjEUtsp5VLZ-9sHilh77Tm-SSd__p6LqK5MVm6gGIpONgr0HCXih9BPc0',
-    'client_secret': 'EKBWwE-DTtYldtlIfwOHSFzZkYEpB3VwWWnrX3rERSQaEw9fBHBtnbnHDhjpQgPiWv99nkZ9b2gEMCb3'
+    'client_id': '',
+    'client_secret': '
   });
 
   
